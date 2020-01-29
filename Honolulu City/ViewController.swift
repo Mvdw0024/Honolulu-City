6b4acf3dae5e2ba9d2fb723270655d9b7f4e542f
Michael Van de Wouwer <M.vdw@live.be>
2020-01-29 09:29:59 +0100

    Initial Commit

---
+++ Honolulu City/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Honolulu City
+//
+//  Created by mobapp08 on 29/01/2020.
+//  Copyright © 2020 mobapp08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
