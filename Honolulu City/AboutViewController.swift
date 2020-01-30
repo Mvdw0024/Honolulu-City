5fe93321575830522d2882acd5cfcb063ce07991
Michael Van de Wouwer <M.vdw@live.be>
2020-01-30 16:48:08 +0100

    made new AboutViewController, adapted classes where needed to keep Map Working

---
+++ Honolulu City/AboutViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.swift
+//  Honolulu City
+//
+//  Created by mobapp08 on 30/01/2020.
+//  Copyright © 2020 mobapp08. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    var images: [String] = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","9.png","11.jpg",]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        
+        for index in 0..<images.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x/scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+    }
+    
+}
